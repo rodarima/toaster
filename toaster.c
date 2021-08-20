a0210c62a76cc6b70ef59c45716e737d9f860bb4
Rodrigo <rodarima@gmail.com>
2021-08-20 17:22:43 +0200

    Implement support for multiple workers

--- toaster.c
+++ toaster.c
@@ -6,6 +6,15 @@
 #include <math.h>
 #include <time.h>
 #include <sys/random.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+struct worker {
+	pthread_t thread;
+	volatile int do_work;
+	volatile int should_die;
+	sem_t sem;
+};
 
 /* Returns a random double from [0, 1] draw
  * from an uniform distribution */
@@ -102,48 +111,112 @@ burn()
 	out = e;
 }
 
-void
-burn_cpu(long us_len)
+void *
+worker_routine(void *arg)
 {
-#define E9 1000000000L
+	struct worker *data;
 
-	long s_len, ns_len;
-	struct timespec end;
-	if(clock_gettime(CLOCK_MONOTONIC, &end) != 0)
+	data = (struct worker *) arg;
+
+	while(1)
 	{
-		perror("clock_gettime");
+		while(data->do_work)
+			burn();
+
+		if(data->should_die)
+			break;
+
+		if(sem_wait(&data->sem) != 0)
+		{
+			perror("sem_wait");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return NULL;
+}
+
+struct worker *
+create_workers(int n)
+{
+	int i;
+	struct worker *workers;
+
+	workers = calloc(n, sizeof(struct worker));
+
+	if(workers == NULL)
+	{
+		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 
-	//printf("now sec=%ld nsec=%ld\n", end.tv_sec, end.tv_nsec);
-
-	ns_len = us_len * 1000;
-	s_len = ns_len / E9;
-	ns_len -= s_len * E9;
-
-	end.tv_nsec += ns_len;
-	end.tv_sec += s_len;
-
-	/* Compute the end timespec by adding us_len */
-	if(end.tv_nsec > E9)
+	for(i=0; i<n; i++)
 	{
-		end.tv_sec += 1;
-		end.tv_nsec -= 1000000000L;
+		if(sem_init(&workers[i].sem, 0, 0) != 0)
+		{
+			perror("sem_init");
+			exit(EXIT_FAILURE);
+		}
+
+		workers[i].do_work = 0;
+		workers[i].should_die = 0;
+
+		if(pthread_create(&workers[i].thread, NULL,
+				worker_routine, &workers[i]) != 0)
+		{
+			perror("pthread_create");
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	//printf("end sec=%ld nsec=%ld\n", end.tv_sec, end.tv_nsec);
+	return workers;
+}
 
-	while(!is_toasted(&end))
+void
+kill_workers(struct worker *workers, int n)
+{
+	int i;
+
+	/* Unset the work flag and notify the workers */
+	for(i=0; i<n; i++)
 	{
-		burn();
+		workers[i].should_die = 1;
+		workers[i].do_work = 0;
+		sem_post(&workers[i].sem);
 	}
-#undef E9
+
+	/* Wait for the workers to die */
+	for(i=0; i<n; i++)
+	{
+		if(pthread_join(workers[i].thread, NULL) != 0)
+		{
+			perror("pthread_join");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void
+burn_cpu(struct worker *workers, int n, long us_len)
+{
+	int i;
+
+	for(i=0; i<n; i++)
+	{
+		workers[i].do_work = 1;
+		sem_post(&workers[i].sem);
+	}
+
+	usleep(us_len);
+
+	for(i=0; i<n; i++)
+		workers[i].do_work = 0;
 }
 
 static void
 usage(int argc, char *argv[])
 {
-	fprintf(stderr, "Usage: %s [-w ms] [-W ms] [-b ms] [-B ms] [-n N]\n",
+	fprintf(stderr, "Usage: %s [-w ms] [-W ms] [-b ms] [-B ms] [-n N] [-t threads]\n",
 			argv[0]);
 	exit(EXIT_FAILURE);
 }
@@ -151,6 +224,7 @@ usage(int argc, char *argv[])
 int
 main(int argc, char *argv[])
 {
+	struct worker *workers;
 	int opt, always=1;
 
 	/* Wait between 5 to 60 seconds before next noise */
@@ -159,11 +233,13 @@ main(int argc, char *argv[])
 	/* Noise duration between 200 and 5000 ms */
 	long burn_min = 200, burn_max = 5000;
 
+	long nthreads = 1;
+
 	/* Actual random times */
 	long wait, burn;
 	long i, loops;
 
-	while((opt = getopt(argc, argv, "w:W:b:B:n:")) != -1)
+	while((opt = getopt(argc, argv, "w:W:b:B:n:t:")) != -1)
 	{
 		switch(opt)
 		{
@@ -183,11 +259,16 @@ main(int argc, char *argv[])
 			case 'B':
 				burn_max = atoi(optarg);
 				break;
+			case 't':
+				nthreads = atoi(optarg);
+				break;
 			default: /* ? */
 				usage(argc, argv);
 				break;
 		}
 	}
+
+	workers = create_workers(nthreads);
 
 	for(i=0; always || i<loops; i++)
 	{
@@ -197,8 +278,10 @@ main(int argc, char *argv[])
 
 		burn = random_interval(burn_min, burn_max);
 		printf("%.3f B %ld\n", get_timestamp(), burn);
-		burn_cpu(burn * 1000);
+		burn_cpu(workers, nthreads, burn * 1000);
 	}
+
+	kill_workers(workers, nthreads);
 
 	return 0;
 }
